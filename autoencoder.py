486aaa6184f9203a92a0657fdb1a1f532ebfc079
Da Yeon Hwang <dahwang2018@u.northwestern.edu>
2017-11-01 23:56:57 -0500

    worked on saving tensorflow model & getting synthesized embedding set

--- autoencoder.py
+++ autoencoder.py
@@ -4,7 +4,6 @@ import pandas as pd
 import tensorflow as tf
 from sklearn import datasets
 from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
 
 # Implementation of a feedforward neural network(MLP) to use it as an autoencoder for word embeddings
 # Requirements: TensorFlow, Pandas, Sklearn, Numpy
@@ -12,10 +11,9 @@ from sklearn.model_selection import train_test_split
 # TO DO:
 # 1. data processing to script
 # 2. expand to 3 (or 5) hidden layers
-# 3. save output from hidden layer as new word embeddings w/o tanh activation function
 
 # set random seed value
-RANDOM_SEED = 50 # Q1. better seed value???
+RANDOM_SEED = 50
 tf.set_random_seed(RANDOM_SEED)
 
 def init_weights(shape):
@@ -28,12 +26,12 @@ def init_weights(shape):
 
 def forward(X, w_1, w_2, b_1, b_2):
 	"""forward-propagation
-	Note: propagate inputs X through network
+	Note: propagate inputs X through network, return output from hidden layer as "new synthesized word embedding"
 	"""
-	h = tf.tanh(tf.add(tf.matmul(X, w_1), b_1)) # h: hidden layer (h = x * w_1 + b_1)
-	y_predict = tf.tanh(tf.add(tf.matmul(h, w_2), b_2)) # y_predict: estimated output (y_hat = h * w_2 + b_2)
+	h = tf.add(tf.matmul(X, w_1), b_1) # h: hidden layer (h = x * w_1 + b_1)
+	y_predict = tf.add(tf.matmul(h, w_2), b_2) # y_predict: estimated output (y_hat = h * w_2 + b_2)
 
-	return y_predict
+	return h, y_predict
 
 def concatenate_all_data(d):
 	"""data pre-processing
@@ -77,7 +75,7 @@ def main():
 	all_embeddings = [govt, books]
 	input_data = concatenate_all_data(all_embeddings) #(19929,201)
 	input_data = input_data[input_data.columns[2:]]
-	input_data.to_csv('data/final.csv')
+	input_data.to_csv('data/input_data.csv')
 
 	# split data into training & testing sets
 	train, test = split_data(input_data) # train: (17936,201)
@@ -90,10 +88,11 @@ def main():
 
 	# set batch size
 	batch = 200 # number of words in each batch => epoch: 100 iterations
+	model_path = "model/model.ckpt" # path to save model
 
 	# set hidden layer size
 	x_size = train_X.shape[1] # number of input nodes 
-	h_size = 100  # number of hidden nodes
+	h_size = 100  # number of hidden nodes(dimension)
 	y_size = train_y.shape[1] # number of outcomes
 
 	# set tf graph input 
@@ -109,70 +108,104 @@ def main():
 	b_2 = tf.Variable(tf.ones([y_size]))
 
 	# forward propagation
-	y_predict = forward(X, w_1, w_2, b_1, b_2)
-	predict = tf.argmax(y_predict, axis=1)
+	hidden_output, y_predict = forward(X, w_1, w_2, b_1, b_2) # hidden_output: synthesized word embedding
 
 	# backward propagation
 	error = tf.subtract(y, y_predict)
 	mse = tf.reduce_mean(tf.square(error))
 	update = tf.train.GradientDescentOptimizer(0.001).minimize(mse) # SGD step size: 0.001
 
+	# create 'saver' op to save all variables
+	saver = tf.train.Saver()
+
 	# run stochastic gradient descent
 	sess = tf.Session()
 	init = tf.global_variables_initializer()
-	sess.run(init)
 
+	with tf.Session() as sess:
+		sess.run(init)
 
-	for epoch in range(100): 
-		# initialize variables / assign values
-		total_mse = 0
-		total_mse_test = 0
-		total_mse_valid = 0
-		train = shuffle(train)
-		test = shuffle(test)
-		train_X = train
-		train_y = train
-		test_X = test
-		test_y = test
-		validation_X = validation
-		validation_y = validation
-		last_train_batch = train_X.shape[0] % batch
-		last_test_batch = test_X.shape[0] % batch
-		last_validation_batch = validation_X.shape[0] % batch
+		tf.train.write_graph(sess.graph, "model/",'graph.pbtxt')
 
-		# compute training accuracy
-		for i in range(0, len(train_X), 200):
-			if (i + batch < len(train_X)):
-				batch_mse, __ = sess.run([mse,update], feed_dict={X: train_X[i: i+batch], y: train_y[i: i+batch]})
-				i = i + batch
-			else: # last batch
-				batch_mse, __ = sess.run([mse,update], feed_dict={X: train_X[i:i+last_train_batch], y: train_y[i:i+last_train_batch]})
-			total_mse = total_mse + batch_mse * batch
-		train_accuracy = total_mse / len(train_X)
+		# tf.summary.scalar("Mean Squared Error:", mse)
+		# tf.summary.histogram('weight 1', w_1)
+		# tf.summary.histogram('bias 1', b_1)
+		# tf.summary.histogram('weight 2', w_2)
+		# tf.summary.histogram('bias 2', b_2)
+		# tf.summary.histogram("Hideen layer output", hidden_output)
+		# summary_op = tf.summary.merge_all()
+		# #summary_writer = tf.train.SummaryWriter('./tenIrisSave/logs',graph_def=sess.graph_def)
+		# #summary_writer = tf.summary.FileWriter('data/',sess.graph)
+		# saver = tf.train.Saver([w_1,b_1])
 
-		# compute testing accuracy
-		for i in range(0, len(test_X), 200):
-			if (i + batch < len(train_X)):
-				batch_mse = sess.run(mse, feed_dict={X: test_X[i: i+batch], y: test_y[i: i+batch]})
-				i = i + batch
-			else:
-				batch_mse = sess.run(mse, feed_dict={X: test_X[i: i+last_test_batch], y: test_y[i: i+last_test_batch]})
-			total_mse_test = total_mse_test + batch_mse * batch
-		test_accuracy = total_mse_test / len(test_X)
+		print("...")
 
-		# compute validation accuracy
-		for i in range(0, len(validation_X), 200):
-			if (i + batch < len(train_X)):
-				batch_mse = sess.run(mse, feed_dict={X: validation_X[i: i+batch], y: validation_y[i: i+batch]})
-				i = i + batch
-			else:
-				batch_mse = sess.run(mse, feed_dict={X: validation_X[i: i+last_validation_batch], y: validation_y[i: i+last_validation_batch]})
-			total_mse_valid = total_mse_valid + batch_mse * batch
-		validation_accuracy = total_mse_valid / len(validation_X)
+		for epoch in range(100): 
+			# initialize variables
+			total_mse = total_mse_test = total_mse_valid = 0
+			train = shuffle(train)
+			test = shuffle(test)
+			train_X = train_y = train
+			test_X = test_y = test
+			validation_X = validation_y = validation
+			last_train_batch = train_X.shape[0] % batch
+			last_test_batch = test_X.shape[0] % batch
+			last_validation_batch = validation_X.shape[0] % batch
 
-		print("Epoch = %d, train accuracy = %.2f%%, test accuracy = %.2f%%, validation accuracy = %.2f%%" % (epoch + 1, train_accuracy, test_accuracy, validation_accuracy))
+			# compute training accuracy (average of L2 norm)
+			for i in range(0, len(train_X), 200):
+				if (i + batch < len(train_X)):
+					batch_mse, __ = sess.run([mse,update], feed_dict={X: train_X[i: i+batch], y: train_y[i: i+batch]})
+					i = i + batch
+				else: # last batch
+					batch_mse, __ = sess.run([mse,update], feed_dict={X: train_X[i:i+last_train_batch], y: train_y[i:i+last_train_batch]})
+				total_mse = total_mse + batch_mse * batch
+			train_accuracy = total_mse / len(train_X)
 
-	sess.close()
+			# compute testing accuracy (average of L2 norm)
+			for i in range(0, len(test_X), 200):
+				if (i + batch < len(train_X)):
+					batch_mse = sess.run(mse, feed_dict={X: test_X[i: i+batch], y: test_y[i: i+batch]})
+					i = i + batch
+				else:
+					batch_mse = sess.run(mse, feed_dict={X: test_X[i: i+last_test_batch], y: test_y[i: i+last_test_batch]})
+				total_mse_test = total_mse_test + batch_mse * batch
+			test_accuracy = total_mse_test / len(test_X)
+
+			# compute validation accuracy (average of L2 norm)
+			for i in range(0, len(validation_X), 200):
+				if (i + batch < len(train_X)):
+					batch_mse = sess.run(mse, feed_dict={X: validation_X[i: i+batch], y: validation_y[i: i+batch]})
+					i = i + batch
+				else:
+					batch_mse = sess.run(mse, feed_dict={X: validation_X[i: i+last_validation_batch], y: validation_y[i: i+last_validation_batch]})
+				total_mse_valid = total_mse_valid + batch_mse * batch
+			validation_accuracy = total_mse_valid / len(validation_X)
+
+			print("Epoch = %d, train average MSE = %.2f%%, test average MSE = %.2f%%, validation average MSE = %.2f%%" % (epoch + 1, train_accuracy, test_accuracy, validation_accuracy))
+			
+		# save model 
+		save_path = saver.save(sess, model_path)
+		print("Model saved in file: %s" % save_path)
+
+		# write file for GraphVisualizer
+		file_writer = tf.summary.FileWriter('model/', sess.graph)
+
+		# print instruction to launch Tensorflow @localhost:6006
+		print("Run the Tensorflow with the command: %s" % ("tensorboard --logdir data/"))
+
+	  	# get output of hidden layer node("Variable") from TensorFlow session
+		layer1_output = tf.get_default_graph().get_tensor_by_name("Variable:0")
+		
+		# save output as a new synthesized word embedding set
+		syn_embedding = sess.run(layer1_output) #(199, 50)
+
+		# close TensorFlow session
+		sess.close()
+
+		# write CSV file for a new embedding set
+		syn_embedding = np.transpose(syn_embedding)
+		pd.DataFrame(syn_embedding).to_csv('data/syn_embedding.csv')
 
 if __name__ == '__main__':
-    main()
+	main()
